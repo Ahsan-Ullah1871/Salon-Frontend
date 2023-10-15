9ce194be42288598ac988863ab32fa9755e7a019
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-10-16 04:03:23 +0600

    - blogs list UI finished

---
+++ app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import Blogs from "@/components/PagesComponents/Blog/BlogsList";
+import Services from "@/components/PagesComponents/Services/ServicesList";
+
+const page = () => {
+	return (
+		<div className="px-6">
+			<Blogs />
+		</div>
+	);
+};
+
+export default page;
+
